6c8e8bc19dcc46a08df80efebee56292703328ad
Alexandre Ménard <1751307@cstj.qc.ca>
2025-03-08 14:41:55 -0500

    changement de la structure

--- admin/plats/index.php
+++ admin/plats/index.php
@@ -25,7 +25,7 @@ $plats = selectAll("plats","*","nom ASC");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/admin.css">
 </head>
 <body>
     <div class="all">
